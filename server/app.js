524d647797b068719d252cd951597b8168b8f8e2
unknown <mdsabbirrahman2025@gmail.com>
2025-06-30 02:43:07 +0600

    test server

--- server/app.js
+++ server/app.js
@@ -1,15 +1,21 @@
-const express = require('express')
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
-const userRoutes = require('./routes/userRoutes')
-const blogRoutes = require('./routes/blogRoutes')
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const userRoutes = require('./routes/userRoutes');
+const blogRoutes = require('./routes/blogRoutes');
 
 const app = express();
-app.use(cors({origin:'http://localhost:5173', credentials:true}));
+
+app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/users', userRoutes);
 app.use('/api/blogs', blogRoutes);
+
+// ✅ Test route
+app.get('/', (req, res) => {
+  res.send('🚀 ThriveSpace Backend is live!');
+});
 
 module.exports = app;
